e7eaf0ffeb74b40e012d462472b904b4e056c48a
Steven Trotter <stevetrot@gmail.com>
2022-04-13 12:10:21 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,56 @@
 mod logger;
+mod util;
 
 use std::{
     cmp,
     collections::HashMap,
-    env,
+    env::{self, consts::EXE_SUFFIX},
     error::Error,
     fmt::Debug,
     path::Path,
+    process::Stdio,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
     },
 };
 
-use anyhow::Result;
+use anyhow::{bail, Result};
 use async_trait::async_trait;
-use nvim_rs::{compat::tokio::Compat, create::tokio as create, Buffer, Handler, Neovim, Value};
-use tokio::{io::Stdout, sync::Mutex};
+use nvim_rs::{
+    compat::tokio::Compat, create::tokio as create, Buffer, Handler, Neovim, Value, Window,
+};
+use reqwest::Url;
+use tokio::{
+    io::Stdout,
+    net::TcpStream,
+    process::{Child, Command},
+    sync::Mutex,
+};
+use util::get_debuggers_dir;
 
 static VADRE_NEXT_INSTANCE_NUM: AtomicUsize = AtomicUsize::new(1);
 
 type VadreResult = Result<Value, Value>;
 
-enum VadreWindowTypes {
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+enum VadreWindowType {
     Code,
     Output,
 }
 
-impl VadreWindowTypes {
+impl VadreWindowType {
     fn buffer_name_prefix(&self) -> &str {
         match self {
-            VadreWindowTypes::Code => "VadreCode",
-            VadreWindowTypes::Output => "VadreOutput",
+            VadreWindowType::Code => "Vadre Code",
+            VadreWindowType::Output => "Vadre Output",
         }
     }
 
     fn type_name(&self) -> &str {
         match self {
-            VadreWindowTypes::Code => "VadreCode",
-            VadreWindowTypes::Output => "VadreOutput",
+            VadreWindowType::Code => "VadreCode",
+            VadreWindowType::Output => "VadreOutput",
         }
     }
 }
@@ -49,70 +61,28 @@ enum VadreDebugger {
 }
 
 #[derive(Clone)]
-struct Debugger {
-    debugger_type: VadreDebugger,
-    id: usize,
-    command: String,
-    command_args: Vec<String>,
+struct NeovimVadreWindow {
     neovim: Neovim<Compat<Stdout>>,
+    instance_id: usize,
+    windows: HashMap<VadreWindowType, Window<Compat<Stdout>>>,
+    buffers: HashMap<VadreWindowType, Buffer<Compat<Stdout>>>,
 }
 
-impl Debugger {
-    fn new(
-        debugger_type: VadreDebugger,
-        id: usize,
-        command: String,
-        command_args: Vec<String>,
-        neovim: Neovim<Compat<Stdout>>,
-    ) -> Self {
-        Debugger {
-            debugger_type,
-            id,
-            command,
-            command_args,
+impl NeovimVadreWindow {
+    fn new(neovim: Neovim<Compat<Stdout>>, instance_id: usize) -> Self {
+        Self {
             neovim,
+            instance_id,
+            buffers: HashMap::new(),
+            windows: HashMap::new(),
         }
     }
 
-    async fn launch(&self) -> VadreResult {
-        self.setup_ui()
-            .await
-            .expect("Expected UI to setup correctly");
-
-        tracing::debug!(
-            "Launching following process with lldb: {:?} -- {:?}",
-            self.command,
-            self.command_args,
-        );
-
-        Ok(Value::from("process launched"))
-    }
-
-    async fn setup_ui(&self) -> Result<()> {
+    async fn create_ui(&mut self) -> Result<()> {
         let eventignore_old = self.neovim.get_var("eventignore").await;
         self.neovim.set_var("eventignore", "all".into()).await?;
 
-        // Check if we need a new tab first
-        let current_buf = self.neovim.get_current_buf().await?;
-        let current_tabpage = self.neovim.get_current_tabpage().await?;
-
-        let modified = current_buf.get_option("modified").await?.as_bool().unwrap();
-        let line_count = current_buf.line_count().await?;
-        let lines = current_buf.get_lines(0, 1, true).await?;
-
-        let window_count = current_tabpage.list_wins().await?.len();
-        let buffer_name = current_buf.get_name().await?;
-
-        // New tab if we're not completely single window empty pane, otherwise just use current
-        if window_count > 1
-            || buffer_name != ""
-            || modified
-            || line_count > 1
-            || lines.get(0).unwrap() != ""
-        {
-            self.neovim.command("tab new").await?;
-            tracing::trace!("setup a new empty tab");
-        }
+        self.check_if_new_tab_needed().await?;
 
         // Now setup the current window which must by construction be empty
         let current_window = self.neovim.get_current_win().await?;
@@ -137,17 +107,21 @@ impl Debugger {
 
         // Window 1 is code
         let window = windows.next().unwrap();
-        self.neovim.set_current_win(&window).await?;
         let buffer = window.get_buf().await?;
-        self.set_vadre_buffer(buffer, VadreWindowTypes::Code)
+        self.neovim.set_current_win(&window).await?;
+        self.set_vadre_buffer(&buffer, VadreWindowType::Code)
             .await?;
+        self.windows.insert(VadreWindowType::Code, window);
+        self.buffers.insert(VadreWindowType::Code, buffer);
 
         // Window 2 is output
         let window = windows.next().unwrap();
         let buffer = window.get_buf().await?;
-        self.set_vadre_buffer(buffer, VadreWindowTypes::Output)
+        self.set_vadre_buffer(&buffer, VadreWindowType::Output)
             .await?;
         window.set_height(output_window_height).await?;
+        self.windows.insert(VadreWindowType::Output, window);
+        self.buffers.insert(VadreWindowType::Output, buffer);
 
         match eventignore_old {
             Ok(x) => self.neovim.set_var("eventignore", x).await?,
@@ -158,28 +132,186 @@ impl Debugger {
         Ok(())
     }
 
-    async fn set_vadre_buffer(
-        &self,
-        buffer: Buffer<Compat<Stdout>>,
-        window_type: VadreWindowTypes,
-    ) -> Result<()> {
-        let buffer_name = format!("{}Win_{}", window_type.buffer_name_prefix(), self.id);
-        let file_type = window_type.type_name();
-        buffer.set_name(&buffer_name).await?;
-        tracing::trace!("0");
-        buffer.set_option("swapfile", false.into()).await?;
-        tracing::trace!("1");
-        buffer.set_option("buftype", "nofile".into()).await?;
-        tracing::trace!("2");
-        buffer.set_option("filetype", file_type.into()).await?;
-        tracing::trace!("3");
-        buffer.set_option("buflisted", false.into()).await?;
-        tracing::trace!("4");
-        buffer.set_option("modifiable", false.into()).await?;
-        tracing::trace!("5");
+    async fn check_if_new_tab_needed(&self) -> Result<()> {
+        // Check if we need a new tab first
+        let current_buf = self.neovim.get_current_buf().await?;
+        let current_tabpage = self.neovim.get_current_tabpage().await?;
+
+        let modified = current_buf.get_option("modified").await?.as_bool().unwrap();
+        let line_count = current_buf.line_count().await?;
+        let lines = current_buf.get_lines(0, 1, true).await?;
+
+        let window_count = current_tabpage.list_wins().await?.len();
+        let buffer_name = current_buf.get_name().await?;
+
+        // New tab if we're not completely single window empty pane, otherwise just use current
+        if window_count > 1
+            || buffer_name != ""
+            || modified
+            || line_count > 1
+            || lines.get(0).unwrap() != ""
+        {
+            self.neovim.command("tab new").await?;
+            tracing::trace!("setup a new empty tab");
+        }
 
         Ok(())
     }
+
+    async fn set_vadre_buffer(
+        &self,
+        buffer: &Buffer<Compat<Stdout>>,
+        window_type: VadreWindowType,
+    ) -> Result<()> {
+        let buffer_name = format!(
+            "{} ({})",
+            window_type.buffer_name_prefix(),
+            self.instance_id
+        );
+        let file_type = window_type.type_name();
+        buffer.set_name(&buffer_name).await?;
+        buffer.set_option("swapfile", false.into()).await?;
+        buffer.set_option("buftype", "nofile".into()).await?;
+        buffer.set_option("filetype", file_type.into()).await?;
+        buffer.set_option("buflisted", false.into()).await?;
+        buffer.set_option("modifiable", false.into()).await?;
+
+        Ok(())
+    }
+}
+
+#[derive(Clone)]
+struct Debugger {
+    debugger_type: VadreDebugger,
+    id: usize,
+    command: String,
+    command_args: Vec<String>,
+    neovim_vadre_window: NeovimVadreWindow,
+    process: Arc<Mutex<Option<Child>>>,
+    tcp_conn: Arc<Mutex<Option<TcpStream>>>,
+}
+
+impl Debugger {
+    fn new(
+        debugger_type: VadreDebugger,
+        id: usize,
+        command: String,
+        command_args: Vec<String>,
+        neovim: Neovim<Compat<Stdout>>,
+    ) -> Self {
+        let neovim_vadre_window = NeovimVadreWindow::new(neovim, id);
+
+        Debugger {
+            debugger_type,
+            id,
+            command,
+            command_args,
+            neovim_vadre_window,
+            process: Arc::new(Mutex::new(None)),
+            tcp_conn: Arc::new(Mutex::new(None)),
+        }
+    }
+
+    #[tracing::instrument(skip(self))]
+    async fn setup(&mut self) -> VadreResult {
+        self.neovim_vadre_window
+            .create_ui()
+            .await
+            .expect("Expected UI to setup correctly");
+
+        let port = util::get_unused_localhost_port();
+
+        tracing::debug!(
+            "Launching following process with lldb: {:?} -- {:?}",
+            self.command,
+            self.command_args,
+        );
+
+        if let Err(e) = self.launch(port).await {
+            return Err(format!("Error launching process: {}", e).into());
+        };
+
+        if let Err(e) = self.tcp_connect(port).await {
+            return Err(format!("Error creating TCP connection to process: {}", e).into());
+        };
+
+        Ok(Value::from("process launched"))
+    }
+
+    #[tracing::instrument(skip(self, port))]
+    async fn launch(&self, port: u16) -> Result<()> {
+        self.download_plugin().await?;
+
+        let mut path = get_debuggers_dir()?;
+        path.push("codelldb");
+        path.push("extension");
+        path.push("adapter");
+        path.push(format!("codelldb{}", EXE_SUFFIX));
+
+        if !path.exists() {
+            bail!("The binary for codelldb.exe doesn't exist, though it should by this point");
+        }
+
+        tracing::trace!("Spawning processs {:?}", path);
+        *self.process.lock().await = Some(
+            Command::new(path)
+                .args(["--port", &port.to_string()])
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()
+                .expect("Failed to spawn debugger"),
+        );
+        tracing::trace!("Spawned");
+
+        Ok(())
+    }
+
+    #[tracing::instrument(skip(self, port))]
+    async fn tcp_connect(&self, port: u16) -> Result<()> {
+        tracing::trace!("Connecting to port {}", port);
+
+        let tcp_conn = TcpStream::connect(format!("127.0.0.1:{}", port)).await?;
+        *self.tcp_conn.lock().await = Some(tcp_conn);
+
+        tracing::trace!("Connected successfully");
+
+        Ok(())
+    }
+
+    #[tracing::instrument(skip(self))]
+    async fn download_plugin(&self) -> Result<()> {
+        let mut path = get_debuggers_dir()?;
+        path.push("codelldb");
+
+        if !path.exists() {
+            // TODO: Popup informing
+
+            let url = Url::parse(&format!(
+                "https://github.com/vadimcn/vscode-lldb/releases/download/{}\
+                         /codelldb-x86_64-{}.vsix",
+                "v1.7.0", "windows"
+            ))?;
+            download_extract_zip(path.as_path(), url).await?;
+        }
+
+        Ok(())
+    }
+}
+
+// We just make this synchronous because although it slows things down, it makes it much
+// easier to do. If anyone wants to make this async and cool be my guest but it seems not
+// easy.
+async fn download_extract_zip(full_path: &Path, url: Url) -> Result<()> {
+    tracing::trace!("Downloading {} and unzipping to {:?}", url, full_path);
+    let zip_contents = reqwest::get(url).await?.bytes().await?;
+
+    let reader = std::io::Cursor::new(zip_contents);
+    let mut zip = zip::ZipArchive::new(reader)?;
+
+    zip.extract(full_path)?;
+
+    Ok(())
 }
 
 impl Debug for Debugger {
@@ -205,7 +337,7 @@ impl NeovimHandler {
         }
     }
 
-    #[tracing::instrument(skip(args, neovim))]
+    #[tracing::instrument(skip(self, args, neovim))]
     async fn launch(
         &self,
         instance_id: usize,
@@ -258,8 +390,8 @@ impl NeovimHandler {
             debugger_type
         );
 
-        let debugger = Debugger::new(debugger_type, instance_id, command, command_args, neovim);
-        debugger.launch().await?;
+        let mut debugger = Debugger::new(debugger_type, instance_id, command, command_args, neovim);
+        debugger.setup().await?;
 
         self.debuggers.lock().await.insert(instance_id, debugger);
 
